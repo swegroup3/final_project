f8f0f452ae99a9e87e2f2a8fcd28dadbff8a16e4
tfonzi <tyler.a.fonzi@gmail.com>
2019-04-01 19:14:32 -0400

    Finshed off Checkout Page

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  cart = []
 
-  constructor() { }
+
+  constructor(private _checkoutService: CartService) { }
 
   ngOnInit() {
+    this._checkoutService.getCart().subscribe(res => {
+			this.cart = res.items;
+		}, console.log);
+  }
+
+  getTotalPrice(){
+
+    let sum = 0;
+    for(let i = 0; i < this.cart.length; i++){
+      let product = this.cart[i].price * this.cart[i].quantity;
+      sum += product;
+    }
+    return sum;
   }
 
 }
